044db763281e67883ff0f6c18380ed3cd5c8cf10
Abhishek Goswami <abi.xa12@gmail.com>
2023-10-09 22:32:02 +0530

    final

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
+  // app.enableCors({
+  //   origin: ['http://localhost:5173'],
+  //   methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  // });
+
   app.enableCors({
-    origin: ['http://localhost:5173'],
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    origin: '*',
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
   });
+
   await app.listen(3000);
 }
 bootstrap();
